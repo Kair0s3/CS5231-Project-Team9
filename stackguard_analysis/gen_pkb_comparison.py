f7f45e13708d4bcc044a8bbe482f0d765ba55a5f
APZH <amospzh@gmail.com>
2022-10-27 01:08:43 +0800

    updated gen_pkb_comparison to generate syscall files using read_trace

--- stackguard_analysis/gen_pkb_comparison.py
+++ stackguard_analysis/gen_pkb_comparison.py
@@ -5,6 +5,12 @@
 #  - line1 is the path to vulnerable.c
 #  - line2 is the path to dynamorio's ddrun
 #  - line3 is the path to peekaboo's libpeekabo_dr.so 
+#  - line4 is the path to read_trace
+#  - line5 is the path to base target
+#  - line6 is the path to 1st modified target (no overflow)
+#  - line7 is the path to 2nd modified target (with overflow)
+
+# /home/ubuntu/Desktop/peekaboo-master/read_trace /home/ubuntu/Desktop/cs5231_tp_repo/stackguard_analysis/modified-13328-1400As/13328/
 
 import os
 
@@ -23,9 +29,10 @@ def createBaseCase():
 	gcc_base_cmd = "gcc -g"
 	gcc_executable_base_name = "base"
 
-	genereateBase = input("[#] Do you want to generate peekaboo trace with no mem-def (i.e., base case)? [Y|n] ")
+	generateBase = input("[#] Do you want to generate peekaboo trace with no mem-def (i.e., base case)? [Y|n] ")
 
-	if(genereateBase == 'Y'):
+	if(generateBase == 'Y'):
+
 		for i in range(4):
 			gcc_base_cmd += " " + gcc_md_flags[i]
 
@@ -39,6 +46,7 @@ def createBaseCase():
 		peekaboo_cmd_base = filepaths[1] + " -c " + filepaths[2] + " -- " + gcc_executable_base_name
 		os.system(peekaboo_cmd_base)
 
+
 def createModifiedCase():
 
 	# ---------------------------------------------
@@ -50,44 +58,132 @@ def createModifiedCase():
 	hasAslr = 0
 	gcc_executable_modified_name = "modified"
 
-	print("\nChoose which memory defences to remove for the modified version...")
-	print("[0] RELRO -- [1] CANARY -- [2] NX -- [3] PIE -- [4] disable ASLR -- [5] enable ASLR\n")
+	generateModified = input("[#] Do you want to generate peekaboo trace with your own mem-def specification? [Y|n] ")
 
-	selected_gcc_flags = input("[#] Enter defence to remove (e.g., 12 == canary + nx): ")
+	if(generateModified == 'Y'):
 
-	selected_gcc_flags_tokens = [int(flag) for flag in str(selected_gcc_flags)]
+		print("\nChoose which memory defences to remove for the modified version...")
+		print("[0] RELRO -- [1] CANARY -- [2] NX -- [3] PIE -- [4] disable ASLR -- [5] enable ASLR\n")
 
-	for flag in selected_gcc_flags_tokens:
-		if flag == 4 or flag == 5:
-			aslr_cmd += gcc_md_flags[flag]
-			hasAslr = 1
-			continue
-		gcc_cmd += " " + gcc_md_flags[flag]
+		selected_gcc_flags = input("[#] Enter defence to remove (e.g., 12 == canary + nx): ")
 
-	print("creating executable: modified")
+		selected_gcc_flags_tokens = [int(flag) for flag in str(selected_gcc_flags)]
 
-	if hasAslr == 1:
-		print(gcc_cmd + filepaths[0] + " -o " + gcc_executable_modified_name + aslr_cmd)
-		os.system(gcc_cmd + " " + filepaths[0] + " -o " + gcc_executable_modified_name + aslr_cmd)
-	else:
-		print(gcc_cmd + filepaths[0] + " -o " + gcc_executable_modified_name)
-		os.system(gcc_cmd + " " + filepaths[0] + " -o " + gcc_executable_modified_name)
+		for flag in selected_gcc_flags_tokens:
+			if flag == 4 or flag == 5:
+				aslr_cmd += gcc_md_flags[flag]
+				hasAslr = 1
+				continue
+			gcc_cmd += " " + gcc_md_flags[flag]
+
+		print("creating executable: modified")
+
+		if hasAslr == 1:
+			print(gcc_cmd + filepaths[0] + " -o " + gcc_executable_modified_name + aslr_cmd)
+			os.system(gcc_cmd + " " + filepaths[0] + " -o " + gcc_executable_modified_name + aslr_cmd)
+		else:
+			print(gcc_cmd + filepaths[0] + " -o " + gcc_executable_modified_name)
+			os.system(gcc_cmd + " " + filepaths[0] + " -o " + gcc_executable_modified_name)
+
+		# ---------------------------------------------
+		# Double check memory defences with 'checksec'
+		# ---------------------------------------------
+
+		print("checksec output: modified")
+		os.system("checksec --file=" + gcc_executable_modified_name)
+
+		# ---------------------------------------------
+		# Create peekaboo trace files (modified)
+		# ---------------------------------------------
+
+		peekaboo_cmd_modified = filepaths[1] + " -c " + filepaths[2] + " -- " + gcc_executable_modified_name
+		os.system(peekaboo_cmd_modified)
+
+
+def compareSyscall():
 
 	# ---------------------------------------------
-	# Double check memory defences with 'checksec'
+	# Create syscall dump of all trace files
 	# ---------------------------------------------
 
-	print("checksec output: modified")
-	os.system("checksec --file=" + gcc_executable_modified_name)
+	base_syscall_filename = "syscall_compare/base_syscall.txt"
+	modified1_syscall_filename = "syscall_compare/noOverflow_syscall.txt"
+	modified2_syscall_filename = "syscall_compare/withOverflow_syscall.txt"
+
+	generateSyscallFiles = input("[#] Do you want to generate peekaboo syscall files? [Y|n] ")
+
+	if(generateSyscallFiles == 'Y'):
+
+		# base
+		print(filepaths[3] + " -y " + filepaths[4])
+		os.system(filepaths[3] + " -y " + filepaths[4] + " > " + base_syscall_filename)
+
+		# modified1 (no overflow)
+		print(filepaths[3] + " -y " + filepaths[5])
+		os.system(filepaths[3] + " -y " + filepaths[5] + " > " + modified1_syscall_filename)
+
+		# modified2 (with overflow)
+		print(filepaths[3] + " -y " + filepaths[6])
+		os.system(filepaths[3] + " -y " + filepaths[6] + " > " + modified2_syscall_filename)
+
+	with open(base_syscall_filename) as f:
+		base_syscalls = [line.rstrip() for line in f]
+
+	with open(modified1_syscall_filename) as f:
+		modified1_syscalls = [line.rstrip() for line in f]
+
+	with open(modified2_syscall_filename) as f:
+		modified2_syscalls = [line.rstrip() for line in f]
 
 	# ---------------------------------------------
-	# Create peekaboo trace files (modified)
+	# Generate syscall_diff_summary
 	# ---------------------------------------------
+	
+	# clean up syscalls (remove inst id)
+	line_iterator = 0
+	del base_syscalls[0:3]
+	del base_syscalls[-1]
 
-	peekaboo_cmd_modified = filepaths[1] + " -c " + filepaths[2] + " -- " + gcc_executable_modified_name
-	os.system(peekaboo_cmd_modified)
+	base_syscalls_num_lines = len(base_syscalls)
+	while line_iterator < base_syscalls_num_lines:
+		index = base_syscalls[line_iterator].find(']') + 1
+		base_syscalls[line_iterator] = base_syscalls[line_iterator][index:].lstrip()
+		line_iterator += 1
+		
+	line_iterator = 0
+	del modified1_syscalls[0:3]
+	del modified1_syscalls[-1]
+
+	modified1_syscalls_num_lines = len(modified1_syscalls)
+	while line_iterator < modified1_syscalls_num_lines:
+		index = modified1_syscalls[line_iterator].find(']') + 1
+		modified1_syscalls[line_iterator] = modified1_syscalls[line_iterator][index:].lstrip()
+		line_iterator += 1
+
+	line_iterator = 0
+	del modified2_syscalls[0:3]
+	del modified2_syscalls[-1]
+
+	modified2_syscalls_num_lines = len(modified2_syscalls)
+	while line_iterator < modified2_syscalls_num_lines:
+		index = modified2_syscalls[line_iterator].find(']') + 1
+		modified2_syscalls[line_iterator] = modified2_syscalls[line_iterator][index:].lstrip()
+		line_iterator += 1
+
+	# base vs modified1		
+
+	# base vs modified2
+
+	# modified1 vs modified2
+
+
 
 
 if __name__ == "__main__":
+
+	# create peekaboo files
 	createBaseCase()
 	createModifiedCase()
+
+	# run read_trace analysis
+	compareSyscall()
